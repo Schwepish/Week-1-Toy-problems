66fec540aae0f49d50e8c5884abe8c5cf8afa14b
schwepish <felix.kahuta@student.moringaschool.com>
2023-10-19 00:29:11 +0300

    done students

---
+++ Challenge-1/index.js
@@ -0,0 +1,13 @@
+const percent = 90;
+
+if(percent >= 79) {
+    grade = "A";
+} else if(percent >= 60 && percent < 79) {
+    grade = "B";
+} else if(percent >= 59 && percent < 49) {
+    grade = "C";
+} else if(percent >= 49 && percent < 40) {
+    grade = "D";
+} else {
+    grade = "E";
+}
